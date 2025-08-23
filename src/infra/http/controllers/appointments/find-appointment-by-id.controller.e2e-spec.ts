03baf0ede647491df43e18d33cb45fb9edf85c95
Didi <diogo.saran@playnext.com>
2025-08-22 21:53:13 -0300

    added missing update on last commit

---
+++ src/infra/http/controllers/appointments/find-appointment-by-id.controller.e2e-spec.ts
@@ -0,0 +1,161 @@
+import { AppModule } from '@/app.module';
+import { JwtEncrypter } from '@/infra/cryptography/jwt-encryptor';
+import { DatabaseModule } from '@/infra/database/database.module';
+import { PrismaService } from '@/infra/database/prisma/prisma.service';
+import { faker } from '@faker-js/faker';
+import { INestApplication } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import { addDays, endOfMonth, startOfMonth } from 'date-fns';
+import request from 'supertest';
+import { AppointmentFactory } from 'test/factories/make-appointment';
+import { OrganizationFactory } from 'test/factories/make-organization';
+import { ServiceFactory } from 'test/factories/make-service';
+import { SpaceOfServiceFactory } from 'test/factories/make-space-of-service';
+import { UserFactory } from 'test/factories/make-user';
+
+describe('List appointments by date range (E2E)', () => {
+  let app: INestApplication;
+  let prisma: PrismaService;
+  let userFactory: UserFactory;
+  let organizationFactory: OrganizationFactory;
+  let spaceOfServiceFactory: SpaceOfServiceFactory;
+  let serviceFactory: ServiceFactory;
+  let appointmentFactory: AppointmentFactory;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule, DatabaseModule],
+      providers: [
+        UserFactory,
+        OrganizationFactory,
+        SpaceOfServiceFactory,
+        ServiceFactory,
+        AppointmentFactory,
+        JwtEncrypter,
+        PrismaService,
+      ],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+    prisma = moduleRef.get(PrismaService);
+    userFactory = moduleRef.get(UserFactory);
+    organizationFactory = moduleRef.get(OrganizationFactory);
+    spaceOfServiceFactory = moduleRef.get(SpaceOfServiceFactory);
+    serviceFactory = moduleRef.get(ServiceFactory);
+    appointmentFactory = moduleRef.get(AppointmentFactory);
+
+    await app.init();
+  });
+
+  test('[GET] /organizations/:organizationId/appointments/list/:startDate/:endDate - should be able to list appointments by date range', async () => {
+    const user = await userFactory.makePrismaUser();
+    const accessToken = await userFactory.makeToken(user.id.toString());
+
+    const organization = await organizationFactory.makePrismaOrganization({
+      ownerId: user.id,
+    });
+
+    const spaceOfService = await spaceOfServiceFactory.makePrismaSpaceOfService(
+      {
+        organizationId: organization.id,
+      },
+    );
+
+    const service = await serviceFactory.makePrismaService({
+      organizationId: organization.id,
+    });
+
+    const date = faker.date.future();
+
+    const firstDay = startOfMonth(date);
+
+    const startDate = firstDay;
+    const endDate = endOfMonth(date);
+
+    await appointmentFactory.makePrismaAppointment({
+      organizationId: organization.id,
+      spaceOfServiceId: spaceOfService.id,
+      serviceId: service.id,
+      clientId: user.id,
+      date: firstDay,
+    });
+
+    await appointmentFactory.makePrismaAppointment({
+      organizationId: organization.id,
+      spaceOfServiceId: spaceOfService.id,
+      serviceId: service.id,
+      clientId: user.id,
+      date: addDays(firstDay, 1),
+    });
+
+    await appointmentFactory.makePrismaAppointment({
+      organizationId: organization.id,
+      spaceOfServiceId: spaceOfService.id,
+      serviceId: service.id,
+      clientId: user.id,
+      date: addDays(firstDay, 2),
+    });
+
+    await appointmentFactory.makePrismaAppointment({
+      organizationId: organization.id,
+      spaceOfServiceId: spaceOfService.id,
+      serviceId: service.id,
+      clientId: user.id,
+      date: addDays(firstDay, 3),
+    });
+
+    const organizationId = organization.id.toString();
+
+    const response = await request(app.getHttpServer())
+      .get(`/organizations/${organizationId}/appointments/list/`)
+      .set('Authorization', `Bearer ${accessToken}`)
+      .query({
+        startDate: startDate.toISOString(),
+        endDate: endDate.toISOString(),
+      });
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body.appointments.length).toBe(4);
+  });
+
+  test('[GET] /organizations/:organizationId/appointments/list/:startDate/:endDate - should not be able to list appointments without required fields', async () => {
+    const user = await userFactory.makePrismaUser();
+    const accessToken = await userFactory.makeToken(user.id.toString());
+    const organization = await organizationFactory.makePrismaOrganization({
+      ownerId: user.id,
+    });
+
+    const organizationId = organization.id.toString();
+
+    const response = await request(app.getHttpServer())
+      .get(`/organizations/${organizationId}/appointments/list/`)
+      .set('Authorization', `Bearer ${accessToken}`)
+      .query({});
+
+    expect(response.statusCode).toBe(400);
+  });
+
+  test('[GET] /organizations/:organizationId/appointments/list/:startDate/:endDate - should not be able to list appointments with invalid date range', async () => {
+    const user = await userFactory.makePrismaUser();
+    const accessToken = await userFactory.makeToken(user.id.toString());
+    const organization = await organizationFactory.makePrismaOrganization({
+      ownerId: user.id,
+    });
+
+    const organizationId = organization.id.toString();
+
+    const startDate = new Date();
+    const endDate = new Date();
+    endDate.setDate(endDate.getDate() - 7); // End date before start date
+
+    const response = await request(app.getHttpServer())
+      .get(`/organizations/${organizationId}/appointments/list/`)
+      .set('Authorization', `Bearer ${accessToken}`)
+      .query({
+        startDate: startDate.toISOString(),
+        endDate: endDate.toISOString(),
+      });
+
+    expect(response.statusCode).toBe(400);
+  });
+});
---
+++ src/infra/http/controllers/appointments/find-appointment-by-id.controller.ts
@@ -0,0 +1,67 @@
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Param,
+  Query,
+} from '@nestjs/common';
+
+import { z } from 'zod';
+import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe';
+import { CurrentUser } from '@/infra/auth/current-user-decorator';
+import { UserPayload } from '@/infra/auth/jwt.strategy';
+import { AppointmentNotAvailableError } from '@/domain/application/use-cases/errors/appointment-not-available-error';
+import { ListAppointmentsByDateRangeUseCase } from '@/domain/application/use-cases/appointments/list-appointments-by-date-range';
+import { AppointmentPresenter } from '../../presenters/appointments-presenter';
+
+// todo: add a filter per organization and check autorization to
+//  perform actions based on user role inside of the organizatio
+
+const listAppointmentsByDateRangeQuerySchema = z.object({
+  startDate: z.coerce.date(),
+  endDate: z.coerce.date(),
+});
+
+type ListAppointmentsByDateRangeQuerySchema = z.infer<
+  typeof listAppointmentsByDateRangeQuerySchema
+>;
+
+@Controller('/organizations/:organizationId/appointments/list')
+export class ListAppointmentsByDateRangeController {
+  constructor(
+    private listAppointmentsByDateRange: ListAppointmentsByDateRangeUseCase,
+  ) {}
+
+  @Get()
+  @HttpCode(200)
+  async handle(
+    @Query(new ZodValidationPipe(listAppointmentsByDateRangeQuerySchema))
+    query: ListAppointmentsByDateRangeQuerySchema,
+    @CurrentUser() user: UserPayload,
+    @Param('organizationId') organizationId: string,
+  ) {
+    const { startDate, endDate } = query;
+    const userId = user.sub;
+
+    const result = await this.listAppointmentsByDateRange.execute({
+      organizationId,
+      startDate,
+      endDate,
+    });
+
+    if (result.isLeft()) {
+      switch (result.value.constructor) {
+        case AppointmentNotAvailableError:
+          throw new BadRequestException(result.value.message);
+        default:
+          throw new BadRequestException(result.value.message);
+      }
+    }
+
+    return {
+      appointments: result.value.appointments.map(AppointmentPresenter.toHTTP),
+    };
+  }
+}
